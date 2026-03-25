ba460ea7e2685b6ac0a813ad6ae3e7a27fc0722d
Kunal Mohnani <kunalmohnani213@gmail.com>
2026-03-25 23:11:18 +0530

    Update quries.sql

--- quries.sql
+++ quries.sql
@@ -1,1 +1,6 @@
 -- Business queries and analysis
+SELECT u.name, m.title, b.seats
+FROM Bookings b
+JOIN Users u ON b.user_id = u.user_id
+JOIN Shows s ON b.show_id = s.show_id
+JOIN Movies m ON s.movie_id = m.movie_id;
