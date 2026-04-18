24a90a1f26ef4e629adf1db5e338816880d79b60
Kunal Mohnani <kunalmohnani213@gmail.com>
2026-04-18 22:18:59 +0530

    Added advanced SQL queries including CASE statements, views, and joins

--- quries.sql
+++ quries.sql
@@ -67,3 +67,42 @@ FROM Bookings b
 JOIN Shows s ON b.show_id = s.show_id
 JOIN Movies m ON s.movie_id = m.movie_id
 GROUP BY m.title;
+
+
+-- 5. Classify bookings based on number of seats
+SELECT booking_id, seats,
+    CASE
+        WHEN seats >= 5 THEN 'Large Booking'
+        WHEN seats >= 3 THEN 'Medium Booking'
+        ELSE 'Small Booking'
+    END AS booking_type
+FROM Bookings;
+
+
+-- 6. Theatre hosting the maximum number of shows
+SELECT t.name, COUNT(s.show_id) AS total_shows
+FROM Theatres t
+JOIN Shows s ON t.theatre_id = s.theatre_id
+GROUP BY t.name
+ORDER BY total_shows DESC
+LIMIT 1;
+
+
+-- 7. Users who have not made any bookings
+SELECT u.name
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id
+WHERE b.booking_id IS NULL;
+
+
+-- 8. Create a view for booking summary
+CREATE VIEW BookingSummary AS
+SELECT u.name, m.title, b.seats
+FROM Bookings b
+JOIN Users u ON b.user_id = u.user_id
+JOIN Shows s ON b.show_id = s.show_id
+JOIN Movies m ON s.movie_id = m.movie_id;
+
+
+-- 9. Retrieve data from the view
+SELECT * FROM BookingSummary;
